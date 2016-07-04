65e7adaa5013a9c5dfe7e15951d0fed1e10217db
Bart <kluczynskib@gmail.com>
2016-07-04 18:44:21 +0200

    completed the fizzbuzz once - encountered unexpected rspec issues

---
+++ lib/fizzbuzz3.rb
@@ -0,0 +1,17 @@
+def fizzbuzz3(number)
+
+  if number%15 == 0
+    return "fizzbuzz"
+
+  elsif number%5 == 0
+    return "buzz"
+
+  elsif number%3 == 0
+    return "fizz"
+
+  else
+    return number
+
+  end
+
+end
