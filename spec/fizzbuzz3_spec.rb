2d1526e7c2c2832af21fa8e2bd7013fa33e02be0
Bart <kluczynskib@gmail.com>
2016-07-04 19:06:58 +0200

    completed the fizzbuzz challenge for the second time

--- spec/fizzbuzz3_spec.rb
+++ spec/fizzbuzz3_spec.rb
@@ -1,1 +1,21 @@
+require "fizzbuzz3"
 
+describe "fizzbuzz3" do
+  it "checks if the number is dividable by 5" do
+    expect(fizzbuzz3(5)).to eq "buzz"
+
+end
+  end
+
+describe "fizzbuzz3" do
+  it "checks if the number is dividable by 3" do
+    expect(fizzbuzz3(3)).to eq "fizz"
+
+end
+  end
+
+  describe "fizzbuzz3" do
+    it "checks if the number is dividable by 5 and 3" do
+      expect(fizzbuzz3(15)).to eq "fizzbuzz"
+end
+  end
