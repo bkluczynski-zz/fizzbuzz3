fafe953d98fd584bee748d4151f9e455dcd7ad1a
Bart <kluczynskib@gmail.com>
2016-07-04 19:36:58 +0200

    iterated over the file three times

---
+++ lib/fizzbuzz3.rb
@@ -0,0 +1,19 @@
+def fizzbuzz3(number)
+
+
+  if number%3 == 0 && number%5 == 0
+    return "fizzbuzz"
+  end
+
+  if number%3 == 0
+    return "fizz"
+  end
+
+  if number%5 == 0
+    return "buzz"
+  end
+
+
+
+
+end
